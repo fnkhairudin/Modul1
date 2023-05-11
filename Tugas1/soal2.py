725c313a211d61a6d2c48647647065a1462f11d5
Faisal Nur Khairudin <107239190+fnkhairudin@users.noreply.github.com>
2023-05-11 07:12:55 +0700

    soal2
    determine the square of a certain number

---
+++ Tugas1/soal2.py
@@ -0,0 +1,6 @@
+if __name__ == "__main__":
+    """ Menghitung nilai kuadrat 
+        dari suatu angka yang dinput oleh user
+    """
+    angka = float(input("Silahkan masukkan angka berapapun: ")) # menggunakan float karena tipe data int juga bisa diinputkan ke dalam tipe data float
+    print(f'kuadrat dari {angka} adalah {angka**2}')
