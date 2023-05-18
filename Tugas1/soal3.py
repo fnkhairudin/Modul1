c4aa6dafa845c2f2ea8b77896072b3c63f500b74
Faisal Nur Khairudin <107239190+fnkhairudin@users.noreply.github.com>
2023-05-18 12:13:29 +0700

    Update soal3.py

--- Tugas1/soal3.py
+++ Tugas1/soal3.py
@@ -2,13 +2,13 @@ if __name__ == "__main__":
     """mengkonversi jumlah hari tertentu ke dalam bentuk:
         'x' tahun 'y' bulan 'z' minggu 'n' hari
     """
-    days_in_week = 7 # hari
-    days_in_month = 30 # hari
-    days_in_year = 360 # hari
-    number_days = 485 # hari
+    daysInWeek = 7 # hari
+    daysInMonth = 30 # hari
+    daysInYear = 360 # hari
+    numberDays = 190 # hari
 
-    years = number_days//days_in_year
-    month = (number_days%days_in_year)//days_in_month
-    weeks = (number_days%days_in_month)//days_in_week
-    days = abs(number_days - (years*days_in_year + month*days_in_month + weeks*days_in_week))
-    print(f'{number_days} hari = {years} tahun {month} bulan {weeks} minggu {days} hari')
+    years = numberDays//daysInYear
+    month = (numberDays%daysInYear)//daysInMonth
+    weeks = (numberDays%daysInMonth)//daysInWeek
+    days = numberDays%daysInYear%daysInMonth%daysInWeek
+    print(f'{numberDays} hari = {years} tahun {month} bulan {weeks} minggu {days} hari')
