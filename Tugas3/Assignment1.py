9f53ec8126758a149fcf08f5845c5a15d3c7ca1a
Faisal Nur Khairudin <107239190+fnkhairudin@users.noreply.github.com>
2023-05-18 13:42:29 +0700

    Update Assignment1.py

--- Tugas3/Assignment1.py
+++ Tugas3/Assignment1.py
@@ -1,19 +1,11 @@
 if __name__ == '__main__':
-    """input the message argument in string type"""
+    import string
     def encryptor(message):
-        inputMessage = message.upper()    
-        import string
-        alphabet = string.ascii_uppercase*2 # supaya ketika index character yg ingin dienkripsikan melebihi 26, maka akan diulang lagi kembali ke character awal, yaitu mulai dari 'A'
-        while True:
-            print('Your message encrypted into:')
-            for i in inputMessage:
-                if i == ' ':
-                    print(' ', end='')
-                if i == '.': #conditional statement untuk jika terdapat '.' makan outputnya tetap akan '.'
-                    print('.', end='')
-                if i == 'Z':
-                    print('', end='')
-                elif alphabet[alphabet.find(i) + 4] == 'D': #conditional statement untuk jika output 'D', maka akan "di-skip" (continue)
-                    continue
-                print(alphabet[alphabet.find(i) + 4], end='')
-            break
+        alphabet = string.ascii_uppercase*2
+        for i in message.upper():
+            if i == ' ':
+                print(' ', end='')
+            elif i == '.':
+                print('.', end='')
+            else:
+                print(alphabet[alphabet.index(i) + 4], end='')
