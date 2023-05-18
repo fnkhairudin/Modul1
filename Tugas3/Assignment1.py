ef6de7538d5ce9bea809b2810a3f7aefd29775ac
Faisal Nur Khairudin <107239190+fnkhairudin@users.noreply.github.com>
2023-05-18 14:03:25 +0700

    Update Assignment1.py

--- Tugas3/Assignment1.py
+++ Tugas3/Assignment1.py
@@ -1,11 +1,11 @@
 if __name__ == '__main__':
     import string
     def encryptor(message):
-        alphabet = string.ascii_uppercase*2
+        alphabet = string.ascii_uppercase
         for i in message.upper():
             if i == ' ':
                 print(' ', end='')
             elif i == '.':
                 print('.', end='')
             else:
-                print(alphabet[alphabet.index(i) + 4], end='')
+                print(alphabet[(alphabet.index(i) + 27)%len(alphabet)], end='') # len(alphabet) = 26, supaya ketika key melebihi len(alphabet), maka indexing akan kembali mulai dari index[0] lagi
